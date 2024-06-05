1ed75beebfa02f6adc15fb8d4c7394ed22bf9061
Cora Marcu <coramarcu@gmail.com>
2024-06-05 17:48:00 +0100

    add Sessions to the Users migration file

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -22,7 +22,7 @@ return new class extends Migration
                 $table->timestamps();
                 $table->integer('requested_tickets')->nullable();
                 // $table->string('password');
-                // $table->rememberToken();
+                $table->rememberToken();
     
             });
         }
@@ -34,14 +34,14 @@ return new class extends Migration
         //     $table->timestamp('created_at')->nullable();
         // });
 
-        // Schema::create('sessions', function (Blueprint $table) {
-        //     $table->string('id')->primary();
-        //     $table->foreignId('user_id')->nullable()->index();
-        //     $table->string('ip_address', 45)->nullable();
-        //     $table->text('user_agent')->nullable();
-        //     $table->longText('payload');
-        //     $table->integer('last_activity')->index();
-        // });
+        Schema::create('sessions', function (Blueprint $table) {
+            $table->string('id')->primary();
+            $table->foreignId('user_id')->nullable()->index();
+            $table->string('ip_address', 45)->nullable();
+            $table->text('user_agent')->nullable();
+            $table->longText('payload');
+            $table->integer('last_activity')->index();
+        });
     }
 
     /**
