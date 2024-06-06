1c43d2608a6e99a020b4d530e1279d3bfb78dc66
Cora Marcu <coramarcu@gmail.com>
2024-06-06 13:37:26 +0100

    display events countries on /events

---
+++ resources/js/Pages/Events/Index.vue
@@ -0,0 +1,19 @@
+<template>
+    Hey Events!
+    <a :href="route('second')">Go to second page.</a>
+
+    <article v-for="event in events" :key="event.id">
+        <h1>{{ event.country }}</h1>
+    </article>
+</template>
+
+<script>
+export default {
+    props: {
+        events: Array,
+    },
+    data() {
+        return {};
+    },
+};
+</script>
--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
+use App\Http\Controllers\EventsController;
 
 Route::get('/', function () {
-    return Inertia::render('Users/Index', [
+    return Inertia::render('Events/Index', [
         'name' => 'Cora',
         
     ]);
@@ -13,3 +14,5 @@ Route::get('/', function () {
 Route::get('/second-link', function () {
     return 'Second link!';
 })->name('second');
+
+Route::get('/events', [EventsController::class,'index'])->name('events.index');
