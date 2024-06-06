99e1330f8da266b48913c71acbf77af53a751f00
Cora Marcu <coramarcu@gmail.com>
2024-06-06 14:27:45 +0100

    display all event data on /events

--- resources/js/Pages/Events/Index.vue
+++ resources/js/Pages/Events/Index.vue
@@ -3,7 +3,21 @@
     <a :href="route('second')">Go to second page.</a>
 
     <article v-for="event in events" :key="event.id">
-        <h1>{{ event.country }}</h1>
+        <h1>{{ event.event_name }}</h1>
+        <div class="meta-info">
+            <div class="times">
+                <p>Starts at: {{ event.start_date_time }}</p>
+                <p>Ends at: {{ event.end_date_time }}</p>
+            </div>
+            <div class="location">
+                <p>Country: {{ event.country }}</p>
+                <p>City: {{ event.city }}</p>
+            </div>
+            <p>
+                You can buy a maximum of {{ event.tickets_per_user }} per user
+                for this event.
+            </p>
+        </div>
     </article>
 </template>
 
