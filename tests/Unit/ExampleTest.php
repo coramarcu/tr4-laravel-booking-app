a4abbe8e2ee0735dbef621b9364d872b9b0cca3d
Cora Marcu <coramarcu@gmail.com>
2024-06-14 19:56:33 +0100

    add empty test

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -2,15 +2,19 @@
 
 namespace Tests\Unit;
 
+use App\Services\TicketService;
 use PHPUnit\Framework\TestCase;
+use App\Models\User;
+use Illuminate\Support\Facades\Log;
 
-class ExampleTest extends TestCase
+
+class TicketServiceTest extends TestCase
 {
     /**
      * A basic test example.
      */
-    public function test_that_true_is_true(): void
+    public function test_that_user_cannot_buy_more_tickets_than_max_per_user(): void
     {
-        $this->assertTrue(true);
+        
     }
 }
