27d451fa927e7e0f2e3ae03fb233a7d64144a40a
Cora Marcu <coramarcu@gmail.com>
2024-06-14 20:37:09 +0100

    start first unit test

--- tests/Unit/TicketServiceTest.php
+++ tests/Unit/TicketServiceTest.php
@@ -15,6 +15,18 @@ class TicketServiceTest extends TestCase
      */
     public function test_that_user_cannot_buy_more_tickets_than_max_per_user(): void
     {
-        
+        $user = new class {
+            public $id = 1;
+        };
+
+        $event = new class {
+            public $id = 10;
+            public $tickets_per_user = 3;
+        };
+
+        $ticketService = new TicketService();
+
+        $this->assertSame(null, $ticketService->add($user, $event->id, 5));
+
     }
 }
