529bd6f017298fe859b11bc998437075c52f069e
nc-james-heggs <james.heggs@northcoders.com>
2024-06-26 19:56:51 +0100

    fix: Sample approach to fixing unit tests

--- app/Services/TicketService.php
+++ app/Services/TicketService.php
@@ -10,29 +10,28 @@ class TicketService {
     protected $eventsRepository;
     protected $ticketsrepository;
 
-    public function __construct(EventsRepository $eventsRepository, TicketsRepository $ticketsRepository) 
+    public function __construct(EventsRepository $eventsRepository, TicketsRepository $ticketsRepository)
     {
         $this->eventsRepository = $eventsRepository;
         $this->ticketsrepository = $ticketsRepository;
     }
 
-    public function add(User $user, $eventId) 
+    public function add($userId, $numberOfTickets, $eventId)
     {
 
         $event = $this->eventsRepository->findById($eventId);
         $maxTicketsPerCustomer = $event->tickets_per_user;
-        $numberOfTickets = $user->requested_tickets;
 
-        if($numberOfTickets <= $maxTicketsPerCustomer) 
+        if($numberOfTickets <= $maxTicketsPerCustomer)
         {
             for($i = 0; $i < $numberOfTickets; $i++) {
-                $this->ticketsrepository->createTicket($eventId, $user->id);
+                $this->ticketsrepository->createTicket($eventId, $userId);
             }
 
             return true;
 
         }
-        else 
+        else
         {
             return false;
         }
